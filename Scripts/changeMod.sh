be7299ab9b73dc69c6c942280aea2a2c1c807a15
lilo <lo.yu.linux@gmail.com>
2012-04-29 16:21:21 +0800

    Implement it by rescursion

--- Scripts/changeMod.sh
+++ Scripts/changeMod.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 ########################################################################
-# Version 1.0
+# Version 2.0
 # auther lilo
 # email lo.yu.linux@gmail.com
-# date:2012-04-28
+# date:2012-04-29
 #
 # This file is used to modify the permissions of the directory and files
+# Update the file by using chmod -R.It's implement in a rescursive way.
 #
 ########################################################################
 x=`ls -al | grep "^d" | cut -d " " -f9 | grep -v "\." | grep "."`
 for i in $x ;do
-pwd
-cd "$i/cn/"
-pwd
-chmod 666 data/ 2>>~/errorlog-20120428
-chmod 666 data/* 2>>~/errorlog-20120428
-chmod 555 sys_manage/ 2>>~/errorlog-20120428
-chmod 555 sys_manage/* 2>>~/errorlog-20120428
-cd -
+	chmod -R 755 "$i"/cn/
+	chmod -R 777 "$i"/cn/data/
+	chmod -R 777 "$i"/cn/special/
+	chmod -R 777 "$i"/cn/uploads/
+	chmod 777 "$i"/cn/
 done
