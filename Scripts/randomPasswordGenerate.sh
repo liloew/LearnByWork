f73ea8c45ee0dc01983b6e51cd132d2f5628b40c
lilo <lo.yu.linux@gmail.com>
2012-05-15 11:16:02 +0800

    add some comments

--- Scripts/randomPasswordGenerate.sh
+++ Scripts/randomPasswordGenerate.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+###########################################
+#
+#	Version 1.0
+#	auther	lilo
+#	E-mail	lo.yu.linux@gmail.com	# priority
+#	E-mail	liushuililuo@126.com
+#	E-mail	564901391@qq.com
+#	date	2012-05-15
+# This scripts can generate the password by
+# special number that somebody input through
+# stdin.
+#
+###########################################
 genpasswd(){
 	local l=$1
 	[ "$l" == "" ]	&& l=16
