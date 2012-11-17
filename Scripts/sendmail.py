1e1582762a6e84f52e81ce1762d0135262703ad0
Luo Li <lo.yu.linux@gmail.com>
2012-11-17 20:45:40 +0800

    Delete comments in the code for debug.

--- Scripts/sendmail.py
+++ Scripts/sendmail.py
@@ -3,40 +3,14 @@
 import sys
 
 import smtplib
-####################################
-#	Add by lilo 2012-11-15 08:58
 from email.mime.multipart import MIMEMultipart
-####################################
 from email.mime.text import MIMEText
 
 from email.message import Message
 from email.header import Header
 from base64 import b64encode
 
-# Debug for this script
-import pdb
-
-# msg = Message()
-
 def main(argv):
-#	for arg in argv:
-#		print arg
-	
-	#pdb.set_trace()
-	
-	#base64stromg = base64.b64encode(sys.argv[1])
-	#base64string = b64encode(sys.argv[1])
-#	header = Header(base64string,'utf-8')
-
-	###################################
-	#	2012-11-15 08:50
-	#body = ''
-	#while 1:
-	#	line = sys.stdin.readline()
-	#	if line == '%\n':
-	#		break
-	#	body = body + line
-	###################################
 
 	line = ''
 	body = ''
@@ -45,36 +19,8 @@ def main(argv):
 		if line == "%\n":
 			break
 		body = body + line
-	##########################################
-	#	Comments 2012-11-15 09:35
-	#for i in range(4,13):
-	#	line = line + sys.argv[i]
-	#msg = MIMEText(line,'plain','utf-8')
-	###########################################
 
-
-	###########################################
-	#	Comments 2012-11-15 09:17
-	#line = ''
-	#msg = MIMEMultipart('alternative')
-	#for i in range(4,13):
-	#	line = MIMEText(sys.argv[i],'plain')
-	
-	#msg.attach(line)
-
-
-
-	#msg = MIMEText(sys.argv[4],'plain','utf-8')
-
-	############################################
-	#	2012-11-15 08:55
-	#msg = MIMEText(body,'plain','utf-8')
-	############################################
-
-	#msg = MIMEText(sys.stdin.readlines(),'plain','utf-8')
 	msg = MIMEText(body,'plain','utf-8')
-	#msg['Subject'] = header
-	#msg['Subject'] = sys.argv[1]
 	
 	senter = sys.argv[2]		# Must be equal to username and username = someone@domain.com
 	receiver = sys.argv[3]
