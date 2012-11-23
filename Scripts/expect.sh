fb390ede8706bd9f00878242709335a257a6f105
Luo Li <lo.yu.linux@gmail.com>
2012-11-23 11:49:24 +0800

    修改使用FTP下载文件

--- Scripts/expect.sh
+++ Scripts/expect.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/expect
-set fid [open /root/secret]
+# 获取文件锁
+set fid [open /home/lilo/secret]
+# 由文件获取密码
 set password [read $fid]
-spawn ssh localhost
+spawn ftp localhost
 expect {
 	-re ".*Are.*.*yes.*no.*" {
 	send "yes\n"
 	exp_continue
 	}
+	-re ".*Name.*.*:" {
+	send "ftp\n"
+	exp_continue
+	}
 	"*?assword:*" {
 		send $password
 		send "\n"
-		interact
+	#	interact
+		exp_continue
 		}
+	"ftp> " {
+	send "get eula.en_US"
+	send "\n"
+	interact
+	}
 }
