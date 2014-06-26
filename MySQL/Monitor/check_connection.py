9a4be3d5d16d4754fa99a7830f022c3e7149faac
LuoLee <luolee.me@gmail.com>
2014-06-26 09:33:15 +0800

    从数据库中获取实例连接信息并获取对应连接数目;且增加后台循环式获取

--- MySQL/Monitor/check_connection.py
+++ MySQL/Monitor/check_connection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from time import sleep
 from base import DB, Encrypt
 
 def main():
@@ -11,11 +12,13 @@ def main():
     rows = db.fetchall()
     for row in rows:
         tmpdb = DB(row[1],row[2],row[3],en.decrypt(row[4]))
-        db.execute("SELECT COUNT(*) FROM INFORMATION_SCHEMA.PROCESSLIST")
-        cnt = db.fetchone()
+        tmpdb.execute("SELECT COUNT(*) FROM INFORMATION_SCHEMA.PROCESSLIST")
+        cnt = tmpdb.fetchone()
         db.execute("""INSERT INTO T_CONNECTION(INSTANCE,CNT)
             VALUES({0}, {1})""".format(row[0], cnt[0]))
         db.commit()
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
+        sleep(30)
