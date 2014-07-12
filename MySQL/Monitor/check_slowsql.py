ba342fbc068153ebbbf269d31fdc00bfeffbbe58
luolee.me <luolee.me@gmail.com>
2014-07-12 22:11:28 +0800

    以JSON格式解析服务端数据

--- MySQL/Monitor/check_slowsql.py
+++ MySQL/Monitor/check_slowsql.py
@@ -30,24 +30,20 @@ def check_slowsql(host,port,user,passwd,db):
             data = s.recv(size)
             if not data: break
             data_collect += data
-        for row in data_collect.split('\n'):
-            insertrow = row.split('\t')
-            if len(insertrow) == 9:
-                subinsertrow = insertrow[1:]
-                subinsertrow.insert(0,inst[0])
-                print inst[0],subinsertrow
-                try:
-                    db.executemany("INSERT INTO T_SLOW(INSTID,CNT,TIME,ROS,USR,SLOWSQL,CHKTIME,SQLHASH,STATE) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)", subinsertrow)
-                except MySQLdb.IntegrityError as e:
-                    print 'row:{0} occur error:{1}'.format(tuple(insertrow[1:]),e)
-                state = db.commit()
-                if not state:
-                    print 'UP ID:{0}'.format(insertrow[0])
-                    tmps = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    tmps.connect((inst[1],inst[3]))
-                    tmps.send('UP ID:{0}'.format(insertrow[0]))
-                    data = tmps.recv(size)
-                    tmps.close()
+        json_collect = json.loads(data_collect)
+        for row in json_collect:
+            try:
+                db.executemany("INSERT INTO T_SLOW(SLOWSQL,CMDTYPE,CHKTIME,SERVERID,INSTID,STATE,USERHOST,OBJID) VALUES(%s,%s,%s)", [ i[1] for i in sorted(row.items())])
+            except MySQLdb.IntegrityError as e:
+                print 'row:{0} occur error:{1}'.format(tuple(insertrow[1:]),e)
+            state = db.commit()
+            if not state:
+                print 'UP ID:{0}'.format(insertrow[0])
+                tmps = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                tmps.connect((inst[1],inst[3]))
+                tmps.send('UP ID:{0}'.format(insertrow[0]))
+                data = tmps.recv(size)
+                tmps.close()
         s.close()
 
 if __name__ == "__main__":
