61719a555a9c72ce2c2e52e8fdfb7cf7a678b938
luolee.me <luolee.me@gmail.com>
2014-08-05 17:04:01 +0800

    直接通过slow_log表获取慢查询日志

--- MySQL/Monitor/check_slowsql_v2.py
+++ MySQL/Monitor/check_slowsql_v2.py
@@ -19,16 +19,35 @@ def check_slow(host,port,user,passwd):
     except MySQLdb.OperationalError as e:
         print e
 
+def truncate_slow(host,port,user,passwd):
+    if host and port and user and passwd:
+        db = DB(host=host,port=port,user=user,passwd=passwd,db="mysql")
+    else:
+        print "You mst give all parmaters as host,port,user,passwd."
+        exit(1)
+    try:
+        db.execute("TRUNCATE mysql.slow_log")
+        db.commit()
+    except MySQLdb.OperationalError as e:
+        print e
+
 def store_slow(results,instid,conn):
     if type(results) != list:
         rows = list(results)
     sql = 'INSERT INTO T_SLOW(INSTID,STARTTIME,LOCKTIME,QUERYTIME,USER_HOST,ROWS_SENT,ROWS_EXAMINED,ONDB,SQL_TEXT,OBJID) VALUES ("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")'
+    conn.execute('SELECT OBJID FROM T_SLOW')
+    objids = conn.fetchall()
+    sets = set(i[0] for i in objids)
     for i in xrange(len(rows)):
         row = rows.pop()
-        #print instid, row
-        print(sql % (instid,time.strftime('%Y-%m-%d %H:%M:%S'),row[3],row[2],row[1],row[4],row[5],row[6],row[10],hashlib.sha1(row[10]).hexdigest()))
-        conn.execute(sql % (instid,time.strftime('%Y-%m-%d %H:%M:%S'),row[3],row[2],row[1],row[4],row[5],row[6],row[10],hashlib.sha1(row[10]).hexdigest()))
+        sqlhash = hashlib.sha1(row[10]).hexdigest()
+        if sqlhash in sets:
+            continue
+        conn.execute(sql % (instid,time.strftime('%Y-%m-%d %H:%M:%S'),row[3],row[2],row[1],row[4],row[5],row[6],row[10],sqlhash))
         conn.commit()
+    if rows == []:
+        return True
+    return False
 
 
 def main():
@@ -41,7 +60,9 @@ def main():
     for inst in insts:
         #rows = check_slow('10.1.2.9',3306,'root','password')
         rows = check_slow(inst[1],inst[2],inst[3],en.decrypt(inst[4]))
-        store_slow(rows,inst[0],monitordb)
+        rtc = store_slow(rows,inst[0],monitordb)
+        if rtc:
+            truncate_slow(inst[1],inst[2],inst[3],en.decrypt(inst[4]))
 
 if __name__ == '__main__':
     main()
